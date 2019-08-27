cbca5bfacf7b4f9d13c7775d4ad1f0b8910f3d39
Holden <bhw0010@auburn.edu>
2019-08-27 11:18:06 -0500

    second commit

---
+++ src/test/java/Finder_Test.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+public class Finder_Test {
+    @Test
+    public void test_addIntegers() {
+        int actualResult = 1;
+        int expectedResult = 1;
+        assertEquals(expectedResult,actualResult);
+    }
+}
